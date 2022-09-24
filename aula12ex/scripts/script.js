793d07011f2392fdfd4ce2eb58fb1109fa17d2dc
Anderson Alencar <joveransu@gmail.com>
2022-09-23 22:11:30 -0300

    Finished desafio

--- aula12ex/scripts/script.js
+++ aula12ex/scripts/script.js
@@ -1,1 +1,16 @@
-alert('ola')
+function checkTime(){
+    var agora = new Date()
+    var hora = agora.getHours()
+    var photo = document.getElementById('photo')
+    var phoras = document.getElementById('phoras')
+    if (hora > -1 && hora < 12){
+        phoras.innerHTML = `Bom dia, são exatamente ${hora} horas!`
+        photo.src = 'fotos/manha.jpg'
+    } else if (hora <= 18 ) {
+        phoras.innerHTML = `Boa tarde, são exatamente ${hora} horas!`
+        photo.src = 'fotos/tarde.jpg'
+    } else {
+        phoras.innerHTML = `Boa noite, são exatamente ${hora} horas!`
+        photo.src = 'fotos/noite.jpg'
+    }
+}
