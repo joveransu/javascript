eadcfe40ac57e290ca14ee6fa4c87191fa8a2050
Anderson Alencar <joveransu@gmail.com>
2022-09-27 20:13:41 -0300

    update script

--- ex015/script.js
+++ ex015/script.js
@@ -1,4 +1,44 @@
 function showImage() {
+    var data = new Date()
+    var agora = data.getFullYear()
+    var nasci = document.getElementById('idade').value
+
+    if(nasci > agora){
+        alert('O ano que você digitou é inválido.')
+        return true
+    }
+
+    var sexo = document.getElementById('m').checked //Se true, ele é homem, se não, mulher]
+    var res = document.getElementById('res')
+    var idade = Number(agora) - Number(nasci)
     var div = document.getElementById('photo')
-    div.innerHTML = '<img src="ansu.jpg" alt="imagem.jpg"></img>'
+
+    if (sexo){
+        res.innerHTML = `Detectamos um <strong>Homem</strong> de <strong>${idade}</strong> anos de idade.`
+        
+        if(idade < 18){
+            //Criança
+            div.innerHTML = '<img src="ansu.jpg" alt="imagem.jpg"></img>'
+        } else if(idade < 50){
+            //Adulto
+            div.innerHTML = '<img src="ansu.jpg" alt="imagem.jpg"></img>'
+        } else {
+            //Velho
+            div.innerHTML = '<img src="ansu.jpg" alt="imagem.jpg"></img>'
+        }
+
+    } else {
+        res.innerHTML = `Detectamos uma <strong>Mulher</strong> de <strong>${idade}</strong> anos de idade.`
+        if(idade < 18){
+            //Criança
+            div.innerHTML = '<img src="ansu.jpg" alt="imagem.jpg"></img>'
+        } else if(idade < 50){
+            //Adulto
+            div.innerHTML = '<img src="ansu.jpg" alt="imagem.jpg"></img>'
+        } else {
+            //Velho
+            div.innerHTML = '<img src="ansu.jpg" alt="imagem.jpg"></img>'
+        }
+    }
+    
 }
