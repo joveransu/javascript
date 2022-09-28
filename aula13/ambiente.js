65601deb41060369e1b0d375a011d281168b67c4
Anderson Alencar <joveransu@gmail.com>
2022-09-28 19:26:55 -0300

    Update ambiente.js

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,11 +1,24 @@
+function newLine(){
+    console.log(' ')
+    console.log(' ')
+}
+
 var c = 1
-while (c <= 6){
+while (c <= 10){
     console.log(`Passo ${c}`)
     c++
 }
+
+newLine()
 
 var c = 1
 do {
     console.log(`Volta ${c}`)
     c++
-}while(c <= 6)
+}while(c <= 10)
+
+newLine()
+
+for(var d=1; d <= 10; d++){
+    console.log(`Loop ${d}`)
+}
